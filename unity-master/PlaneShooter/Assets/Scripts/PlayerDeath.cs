aea66aa3a5027c4c747617bcd0850ffcc4fbc057
Herdinai Viktor <h763796@stud.u-szeged.hu>
2019-11-01 12:16:04 +0100

    final

---
+++ unity-master/PlaneShooter/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private HealthSystem hs;
+    private void Start()
+    {
+        hs = this.gameObject.GetComponent<HealthSystem>();
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            col.gameObject.SetActive(false);
+            hs.DealDamage(1);
+            if(hs.health <= 0)
+            {
+
+                GameObject.FindGameObjectWithTag("GameController").GetComponent<RespawnController>().respawn();
+            }
+            
+        } else if (col.gameObject.tag == "spikyObj")
+        {
+
+            this.gameObject.SetActive(false);
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<RespawnController>().respawn();
+            
+        }
+    }
+}
---
+++ unity-master/PlaneShooter/Assets/Scripts/Upgrader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrader : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private FireBullet missile;
+    public float horizontalSpeed, verticalSpeed;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        missile = GameObject.FindGameObjectWithTag("Player").GetComponent<FireBullet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.rb.velocity = new Vector2(horizontalSpeed, verticalSpeed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (!missile.isUpgraded)
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<FireBullet>().isUpgraded = true;
+                this.gameObject.SetActive(false);
+            } else
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<FireBullet>().ReloadUpgraded();
+                this.gameObject.SetActive(false);
+                   
+            }
+            
+        }
+    }
+}
